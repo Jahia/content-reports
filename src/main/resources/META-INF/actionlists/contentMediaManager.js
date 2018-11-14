a1cafda8c631552de145c0ca687c14c38a518320
tdraier <tdraier@jahia.com>
2018-11-14 16:01:04 +0100

    BACKLOG-8973 : udpate cmm integration

--- src/main/resources/META-INF/actionlists/contentMediaManager.js
+++ src/main/resources/META-INF/actionlists/contentMediaManager.js
@@ -1,12 +1,9 @@
-contextJsParameters['config'].actions = Object.assign(contextJsParameters['config'].actions, {
-    "content-reports" : {
-        component: "routerAction",
-        mode: "apps",
-        iframeUrl : ":context/cms/:frame/:workspace/:lang/sites/:site.content-reports.html",
-        target : ["leftMenuManageActions"],
-        requiredPermission: "contentReports",
-        requireModuleInstalledOnSite: 'content-reports',
-        labelKey: 'content-reports:label.contentManager.leftMenu.manage.contentReports.title',
-        icon: 'chart-bar'
-    }
+actionsRegistry.add("content-reports", actionsRegistry.get("router"), {
+    buttonLabel: 'content-reports:label.contentManager.leftMenu.manage.contentReports.title',
+    mode: "apps",
+    iframeUrl : ":context/cms/:frame/:workspace/:lang/sites/:site.content-reports.html",
+    target : ["leftMenuActions"],
+    requiredPermission: "contentReports",
+    requireModuleInstalledOnSite: 'content-reports',
+    icon: 'chart-bar'
 });
