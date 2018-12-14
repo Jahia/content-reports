6ce69a280dc16cf02fe5f87272b8d5a29b43f9e9
dgaillard <dgaillard@jahia.com>
2018-12-14 15:42:00 +0100

    BACKLOG-9227 fix icon

--- src/main/resources/META-INF/actionlists/contentMediaManager.js
+++ src/main/resources/META-INF/actionlists/contentMediaManager.js
@@ -5,5 +5,7 @@ actionsRegistry.add("content-reports", actionsRegistry.get("router"), {
     target : ["leftMenuManageActions"],
     requiredPermission: "contentReports",
     requireModuleInstalledOnSite: 'content-reports',
-    icon: 'chart-bar'
+    buttonIcon: '<svg viewBox="0 0 24 24">' +
+                    '<path d="M22,21H2V3H4V19H6V10H10V19H12V6H16V19H18V14H22V21Z" />' +
+                '</svg>'
 });
